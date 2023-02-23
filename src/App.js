6277622c064b0835c8636c27c24dc62eec525350
naathlje <111729350+naathlje@users.noreply.github.com>
2023-02-23 18:43:20 +0100

    Lagde main.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import './css/main.css'
 import H1 from './components/H1';
+import mainContent from './components/main';
 
 function App() {
   return (
---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import H1 from "./H1"
+export default function mainContent({}){
+return(
+    <main>
+
+    <header>
+    <H1/>
+    </header>
+    <article>
+
+    
+    </article>
+    </main>
+)
+}
