907ffe60f84846c5e34c835a77291cd8c430fc04
naathlje <111729350+naathlje@users.noreply.github.com>
2023-02-24 12:45:01 +0100

    Fikset main.js inn i components branch

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import H1 from "./H1"
+export default function mainContent({}){
+return(
+    <main>
+
+    <header>
+    <H1/>
+    </header>
+    <article>
+        
+
+    
+    </article>
+    </main>
+)
+}
